892c5d94d55acd76d9820bcd01c0576fd34eb6ec
AJ <andrew_jones@outlook.com>
2016-03-24 22:25:47 +0000

    Changing some nonsense

--- UserService.Tests/TestExample.fs
+++ UserService.Tests/TestExample.fs
@@ -5,6 +5,6 @@ open FsUnit
 [<TestFixture>]
   type fixture ()=
     [<Test>]member x.
-         ``when I ask whether it is On it answers false.`` ()=
+         ``1 is 1`` ()=
                 1 |> should equal 1
         
