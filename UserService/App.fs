f135585ebe0131837165d57d44f44d17da3a3486
AJ <andrew_jones@outlook.com>
2016-03-24 23:33:24 +0000

    Adding System.Runtime.Serialization on the mac by referencing a mono lib, intrested to see if it still builds on windows

--- UserService/App.fs
+++ UserService/App.fs
@@ -4,6 +4,26 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
+open Suave.Json
+open System.Runtime.Serialization
+
+type Result<'TSuccess,'TFailure> = 
+  | Success of 'TSuccess
+  | Failure of 'TFailure
+
+[<DataContract>]
+type CreateRequest = 
+  {
+  [<field: DataMember(Name = "firstName")>]
+  firstName:string; 
+  [<field: DataMember(Name = "email")>]
+  email:string
+  }
+
+let validateInput input =
+  if input.firstName = "" then Failure "Name must not be blank"
+  else if input.email = "" then Failure "Email must not be blank"
+  else Success input  // happy path
 
 let app =
   choose
@@ -11,8 +31,10 @@ let app =
         [ path "/hello" >=> OK "Hello GET"
           path "/goodbye" >=> OK "Good bye GET" ]
       POST >=> choose
-        [ path "/hello" >=> OK "Hello POST"
-          path "/goodbye" >=> OK "Good bye POST" ] ]
+        [ path "/create" >=> OK "Hello POST"
+          path "/goodbye" >=> OK "Good bye POST" ]
+      //POST >=> request(fun r -> OK <| deserialiseHere r.form <| validateInput )
+       ]
 
 
 [<EntryPoint>]
